174989d6b83b74bf17aab5f2fa595a69128e8667
Nelson Burton <nelsonb@gmail.com>
2016-07-22 16:50:24 -0400

    refactoring

---
+++ src/main/java/com/controller/PersonController.java
@@ -0,0 +1,58 @@
+package com.controller;
+
+import com.dao.PersonDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.model.Person;
+
+@Controller
+public class PersonController {
+
+
+    @Autowired
+    private PersonDAO personDAO;
+	
+	@RequestMapping(value = "/persons", method = RequestMethod.GET)
+	public String listPersons(Model model) {
+		model.addAttribute("person", new Person());
+		model.addAttribute("listPersons", this.personDAO.listPersons());
+		return "person";
+	}
+	
+	//For add and update person both
+	@RequestMapping(value= "/person/add", method = RequestMethod.POST)
+	public String addPerson(@ModelAttribute("person") Person p){
+		
+		if(p.getId() == 0){
+			//new person, add it
+			this.personDAO.addPerson(p);
+		}else{
+			//existing person, call update
+			this.personDAO.updatePerson(p);
+		}
+		
+		return "redirect:/persons";
+		
+	}
+	
+	@RequestMapping("/remove/{id}")
+    public String removePerson(@PathVariable("id") int id){
+		
+        this.personDAO.removePerson(id);
+        return "redirect:/persons";
+    }
+ 
+    @RequestMapping("/edit/{id}")
+    public String editPerson(@PathVariable("id") int id, Model model){
+        model.addAttribute("person", this.personDAO.getPersonById(id));
+        model.addAttribute("listPersons", this.personDAO.listPersons());
+        return "person";
+    }
+	
+}
